b4ad27c19499f2e753335805bede430099c6c320
hugofbarboza <90349158+Hugofbarboza@users.noreply.github.com>
2023-09-03 10:57:23 -0300

    Add files via upload

---
+++ HandsOn/08 - FUNÇÕESAGREG.sql
@@ -0,0 +1,19 @@
+/* ### FUNÇÕES DE AGREGAÇÃO ### 
+
+MAX - A função MAX() retorna o maior valor da coluna selecionada.
+MIN - A função MIN() retorna o menor valor da coluna selecionada.
+AVG - A função AVG() retorna a média dos valores de uma coluna numérica.
+SUM - A função SUM() retorna a soma dos valores de uma coluna numérica.
+
+
+
+
+
+
+** SINTAXE
+
+*/
+
+-- 1 Entrar no bando de dados AdventureWorks2017
+USE AdventureWorks2017
+GO
