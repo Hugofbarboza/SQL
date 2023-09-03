7c469983208a82afd8b7f9021e69d305230a55ba
hugofbarboza <90349158+Hugofbarboza@users.noreply.github.com>
2023-09-03 10:50:43 -0300

    Add files via upload

--- HandsOn/07 - BETWEEN.sql
+++ HandsOn/07 - BETWEEN.sql
@@ -73,3 +73,76 @@ Existem dois curingas frequentemente usados em conjunto com o operador LIKE:
   O sinal de porcentagem (%) representa zero, um ou vários caracteres
   O sinal de sublinhado (_) representa um único caractere
  */
+
+ --8 Exercícios
+
+
+ --8.1 - Quantos produtos temos cadastrados no sistema que custam mais que 1500 dólares?
+SELECT TOP 10 *
+FROM Production.Product
+GO 
+
+SELECT COUNT(ListPrice) AS quantidadeProdutos
+FROM Production.Product
+WHERE ListPrice > 1500
+GO
+
+--8.2 Quantas pessoas temos com o sobrenome que inicia com a letra P?
+SELECT COUNT(LastName) AS quantidade
+FROM Person.Person
+WHERE LastName LIKE('P%')
+GO
+
+SELECT FirstName, LastName
+FROM Person.Person
+WHERE LastName LIKE 'P%'
+GO
+
+-- 8.3 Em quantas cidades únicas estão cadastrados os clientes?
+
+SELECT TOP 10 *
+FROM Person.Address
+GO
+
+SELECT COUNT(DISTINCT City) AS qtdCidades
+FROM Person.Address
+GO
+
+
+--8.4 Quais são as cidades únicas cadastradas no sistema
+
+SELECT TOP 10 *
+FROM Person.Address
+GO
+
+SELECT DISTINCT City
+FROM Person.Address
+GO
+
+--8.5 Quantos produtos vermelhos têm preço entre 500 a 1000 dólares?
+
+SELECT TOP 10 *
+FROM Production.Product
+GO
+
+SELECT COUNT(ProductID) as quantidade
+FROM Production.Product
+WHERE Color = 'Red' 
+	AND ListPrice BETWEEN 500 AND 1000
+GO
+
+-- 8.6 Quantos produtos cadastrados têm a palavra 'road' no nome deles?
+
+SELECT TOP 10 *
+FROM Production.Product
+GO
+
+SELECT Name
+FROM Production.Product
+WHERE Name LIKE '%road%'
+GO
+
+SELECT COUNT(Name) AS quantidade
+FROM Production.Product
+WHERE Name LIKE '%road%'
+GO
