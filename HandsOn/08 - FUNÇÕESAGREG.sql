2ed6ec1d60453e281563567e9b99c751b99acb5a
hugofbarboza <90349158+Hugofbarboza@users.noreply.github.com>
2023-09-16 14:22:11 -0300

    Add files via upload

--- HandsOn/08 - FUNÇÕESAGREG.sql
+++ HandsOn/08 - FUNÇÕESAGREG.sql
@@ -5,15 +5,29 @@ MIN - A função MIN() retorna o menor valor da coluna selecionada.
 AVG - A função AVG() retorna a média dos valores de uma coluna numérica.
 SUM - A função SUM() retorna a soma dos valores de uma coluna numérica.
 
-
-
-
-
-
 ** SINTAXE
 
 */
 
 -- 1 Entrar no bando de dados AdventureWorks2017
 USE AdventureWorks2017
+GO
+
+-- 2 Somando todos os valores totais (Line Total) tabela Sales.SalesOrderDetail
+SELECT TOP 10 *
+FROM Sales.SalesOrderDetail
+GO
+
+SELECT SUM(LineTotal) AS somaTotal
+FROM Sales.SalesOrderDetail
+GO
+
+-- 3 Menor valor da coluna Line Total tabela Sales.SalesOrderDetail
+SELECT MIN(LineTotal) AS menorValor
+FROM Sales.SalesOrderDetail
+GO
+
+-- 4 Valor médio coluna Line Total tabela Sales.SalesOrderDetail
+SELECT AVG(LineTotal) AS mediaValor
+FROM Sales.SalesOrderDetail
 GO
